6d07d15e729d014ca6a4df5fdf48cd0ab67af24e
Andrey <andreyv@st.tcb.ac.il>
2023-12-12 15:39:05 +0200

    added btn

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ export default function Main() {
   return (
     <>
       <h3> Hello World </h3>
-      <h4> test </h4>
+          <h4> test </h4>
+          <button>ClickMe</button>
     </>
   );
 }
